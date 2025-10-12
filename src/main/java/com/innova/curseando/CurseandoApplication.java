51b5c2ebeea86bedaa16be09e7427b34ab818c34
Steven77dev <steven77dev@gmail.com>
2025-10-11 23:31:01 -0500

    feat: proyecto springboot initializr

---
+++ src/main/java/com/innova/curseando/CurseandoApplication.java
@@ -0,0 +1,13 @@
+package com.innova.curseando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseandoApplication.class, args);
+	}
+
+}
